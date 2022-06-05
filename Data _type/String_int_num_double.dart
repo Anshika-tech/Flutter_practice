6496bc77c6230a041b199e7947e85ff5fec866eb
Anshika-tech <srivastavanshika10@gmail.com>
2022-06-05 10:37:39 +0530

    data type

---
+++ Data _type/String_int_num_double.dart
@@ -0,0 +1,17 @@
+void main(){
+  //String
+  String name="Flutter course";
+  String Training="app development";
+  print("$Training : $name");
+  print(Training +" :"+name);
+  //num
+  num cost=1200;
+  print(cost);
+  //boolean
+  bool a=12<6;
+  print(a);
+  //double
+  double c=12.5;
+  double d=15.7;
+  print(c-d);
+}
